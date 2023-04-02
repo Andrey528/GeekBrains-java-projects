149cd075efce6d522e598b64c145e9ea0fc0386a
Andrew <telepnev.1997@mail.ru>
2023-04-02 17:31:57 +0700

    java_hw_2

--- src/main/java/program.java
+++ src/main/java/program.java
@@ -1,4 +1,4 @@
-import homeWork1.Task3.Task3;
+import semLesson2.Task1.Task1;
 
 public class program {
     public static void main(String[] args) {
@@ -6,9 +6,16 @@ public class program {
 //        semLesson1.Task2.Task2.hello();
 //        int[] array = new int[] {1,1,0,1,1,1};
 //        System.out.println(semLesson1.Task3.Task3.getMaxOneCount(array));
-        homeWork1.Task1.Task1.sumOfNums();
-        homeWork1.Task2.Task2.callFactorial();
-        System.out.println(Task3.findSimpleNums(1000));
-        homeWork1.Task4.Task4.calc();
+//        homeWork1.Task1.Task1.sumOfNums();
+//        homeWork1.Task2.Task2.callFactorial();
+//        System.out.println(Task3.findSimpleNums(1000));
+//        homeWork1.Task4.Task4.calc();
+//        System.out.println(semLesson2.Task1.Task1.getRepeatString(5));
+//        System.out.println(semLesson2.Task1.Task1.getRepeatString(5).length());
+//        System.out.println(semLesson2.Task2.Task2.stringCompression("aaaabbbcdd"));
+//        System.out.println(semLesson2.Task3.Task3.writeString("test"));
+        homeWork2.Task1.Task1.bubbleSort(homeWork2.Task1.Task1.genArray(10));
+        String jsonString = "[{\"фамилия\":\"Иванов\",\"оценка\":\"5\",\"предмет\":\"Математика\"}, {\"фамилия\":\"Петрова\",\"оценка\":\"4\",\"предмет\":\"Информатика\"}, {\"фамилия\":\"Краснов\",\"оценка\":\"5\",\"предмет\":\"Физика\"}]";
+        homeWork2.Task2.Task2.parseJsonString(jsonString);
     }
 }
