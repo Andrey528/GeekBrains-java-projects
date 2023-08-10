ab18955dc26e1bdb904ceae7cc72f3949c310a14
Andrew <telepnev.1997@mail.ru>
2023-08-10 15:44:55 +0700

    gb_algorithm_linkedList added

---
+++ src/main/java/javaAlgorithm/linkedList/Main.java
@@ -0,0 +1,19 @@
+package javaAlgorithm.linkedList;
+
+import javaAlgorithm.linkedList.models.LinkedList;
+
+public class Main {
+    public static void main(String[] args) {
+        LinkedList<Integer> list = new LinkedList<>();
+
+        list.add(5);
+        list.add(2);
+        list.add(1);
+        list.add(8);
+        list.add(9);
+
+        list.print();
+        list.quickSort();
+        list.print();
+    }
+}
